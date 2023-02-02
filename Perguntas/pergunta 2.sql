0d5c4f9329616755b3ca526743608d7325901f2f
Alexsandra2223 <motaale631@gmail.com>
2023-02-02 16:06:06 -0300

    Perguntas

---
+++ Perguntas/pergunta 2.sql
@@ -0,0 +1,2 @@
+SELECT * FROM states ;
+SELECT uf, `cities count` FROM states WHERE `cities count` < 100;
---
+++ Perguntas/pergunta 3.sql
@@ -0,0 +1,3 @@
+SELECT * from states ;
+
+select uf, poverty, region from states where region in ("north", "northeast", "center-west", "southeast", "south");
