3133cf048a23152b07339fa234236b5b7f1e6292
Alexsandra Mota <113552409+Alexsandra2223@users.noreply.github.com>
2023-02-03 16:31:27 -0300

    Add files via upload

---
+++ Perguntas/pergunta 4.sql
@@ -0,0 +1,2 @@
+SELECT * FROM states;
+SELECT uf, gdp FROM states order by gdp desc limit 5
---
+++ Perguntas/pergunta 5.sql
@@ -0,0 +1,2 @@
+SELECT * FROM states;
+SELECT uf, population FROM states order by population desc limit 10;
